b6f3150b3966221e2fb081d236895a8aeda9d4cc
arina <arinasorokina825@gmail.com>
2023-06-14 19:57:44 +0300

    Adds function isPalindrom

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,16 @@ function measureString(string, maxLength) {
 }
 
 console.log(measureString('проверяемая строка', 10));
+
+function isPalindrom(string) {
+  const cleanString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+
+  for (let i = cleanString.length - 1; i >= 0; i--) {
+    reversedString += cleanString.at(i);
+  }
+  return reversedString === cleanString;
+}
+
+console.log(isPalindrom('Лёша на полке клопа нашёл'));
+
