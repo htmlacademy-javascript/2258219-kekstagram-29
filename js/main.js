3b24fed7ba8e2ab7dd61cd84c659c55e9c12edbd
arina <arinasorokina825@gmail.com>
2023-07-27 04:05:31 +0300

    mend

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { getAllPosts } from './data.js';
 import { renderGallery } from './gallery.js';
 import { sendForm } from './upload-form.js';
-
+import './api.js';
 sendForm();
 const posts = getAllPosts();
 renderGallery(posts);
