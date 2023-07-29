ac1f9444dc879f0bc4e58b973726930e84b66218
arina <arinasorokina825@gmail.com>
2023-07-29 15:13:59 +0300

    Adds the new util functions

--- js/util.js
+++ js/util.js
@@ -55,10 +55,31 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this,rest), timeoutDelay);
+  };
+};
+
+const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
+
 export {
   getRandomArrayElement,
   createRandomIdFromRangeGenerator,
   createIdGenerator,
   getRandomInteger,
-  showAlert
+  showAlert,
+  debounce,
+  throttle
 };
