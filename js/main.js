2159ffa7c724818c5acb83c4996d38b0ec5ff6ed
arina <arinasorokina825@gmail.com>
2023-07-29 15:32:14 +0300

    Showing the post's filter

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@ import { getData } from './api.js';
 import { showAlert } from './util.js';
 import { hideModal } from './upload-form.js';
 
+const imageSort = document.querySelector('.img-filters');
 getData()
   .then((posts) => {
     renderGallery(posts);
+  })
+  .then(() => {
+    imageSort.classList.remove('img-filters--inactive');
   })
   .catch(
     (err) => {
