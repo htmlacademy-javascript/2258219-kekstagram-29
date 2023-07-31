5b00cf54e683511c446fccb42fab8c8e0d46e941
arina <arinasorokina825@gmail.com>
2023-07-31 18:15:15 +0300

    mend

--- js/posts-filter.js
+++ js/posts-filter.js
@@ -1,19 +1,3 @@
-/*
-Добавьте обработчики изменения фильтров,
-которые будут управлять порядком отрисовки элементов на странице:
-
-По умолчанию — фотографии в изначальном порядке с сервера. img-filters__button--active" id="filter-default"
-Случайные — 10 случайных, не повторяющихся фотографий. id="filter-random
-Обсуждаемые — фотографии, отсортированные в порядке id="filter-discussed"
-убывания количества комментариев.
-При переключении фильтра все фотографии,
-отрисованные ранее, нужно убрать и вместо них показать те,
-которые подходят под новые условия.
-
-        <button type=button class="img-filters__button  img-filters__button--active" id="filter-default">По умолчанию</button>
-        <button type=button class="img-filters__button" id="filter-random">Случайные</button>
-        <button type=button class="img-filters__button" id="filter-discussed">Обсуждаемые</button>
-*/
 import { fillPostInfo } from './posts.js';
 
 const RANDOM_POSTS_COUNT = 10;
